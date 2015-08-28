0db5a9b81b4593d932f352c4a27e82ef7de40963
inbaloz <ozinbal1@gmail.com>
2015-08-28 21:55:00 +0300

    removed unnecessary macros

--- cnt_cimulation/FindInteracting.h
+++ cnt_cimulation/FindInteracting.h
@@ -3,11 +3,6 @@
 
 #include "Constants_and_libraries.h"
 
-#define SQRT3 (1.7320508075688772)
-#define G_HEIGHT (GRAPHENE_BL * SQRT3)
-#define BN_HEIGHT (BN_LATTICE_BL * SQRT3)
-
 double FindInteracting(Atom atom, double xShift, double yShift, int latticeType);
-
 
 #endif
