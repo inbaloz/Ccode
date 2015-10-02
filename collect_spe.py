e9e844231b7fc07623f7b9c12595a6b5f51dee7d
inbaloz <inbaloz@gandalf2.tau.ac.il>
2015-10-02 15:40:01 +0300

    changed for Itai's new code - KC

--- collect_spe.py
+++ collect_spe.py
@@ -2,13 +2,14 @@ from glob import glob
 import os
 import re
 
-path = r'/home/inbaloz/Projects/NT_on_surfaces/KC_RI_Itai/'
+path = r'/home/inbaloz/Projects/NT_on_surfaces/interlayer_potential_Itai/'
 g = glob(path+"SPE*dat")
 g.sort(key=lambda x: int(x.split(".")[-2]))
 print g
 l = [open(i,'rb').read() for i in g]
 
-ris = [re.findall("E_KC= (.*) RI= (.*)", i.splitlines()[0])[0] for i in l]
-print "\n".join("\t".join(x) for x in ris)
+#ris = [re.findall("E_KC= (.*) RI= (.*)", i.splitlines()[1])[0] for i in l]
+ris = [re.findall("ToT_Energy= (.*)", i.splitlines()[0])[0] for i in l]
+print "\n".join(ris)
 
 
