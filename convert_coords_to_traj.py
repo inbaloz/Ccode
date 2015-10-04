7d568fd36d4dc455da7e25107a94013b95f25197
inbaloz <ozinbal1@gmail.com>
2015-10-04 18:14:29 +0300

    Conversion to trajectories specifies atom type

--- convert_coords_to_traj.py
+++ convert_coords_to_traj.py
@@ -1,10 +1,14 @@
 import os
 import sys
 
+number_to_atom = {'5': 'B', '6': 'C', '7': 'N'}
+
 def convert_coords_to_traj(path):
 	atoms = open(path,'rb').read().splitlines()[1:]
 	new_traj_text = "%d\n0.0000000000000000\n" % len(atoms)
-	new_traj_text += "\n".join("C " + " ".join(atom.split()[2:]) for atom in atoms)
+	new_traj_text += "\n".join(
+		number_to_atom[atom[0]] + " " + " ".join(atom.split()[2:]) for atom in atoms
+	)
 	open(path+".xyz", 'wb').write(new_traj_text)
 
 def main():
