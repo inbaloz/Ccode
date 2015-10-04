adad34479219600ceb586cd2faee3d426fafcc8d
inbaloz <ozinbal1@gmail.com>
2015-10-04 18:13:29 +0300

    Gaussian normalization extention
    - BNNT on h-BN.
    - Heterojunctions.

--- cnt_cimulation/main.c
+++ cnt_cimulation/main.c
@@ -251,77 +251,93 @@ int main(int argc, char *argv[])
 	// RotateShift(tube, tubeN, -M_PI, 0, ILD + radius);
 
 
-    // ----------------------- Assuming the tube is whole.-------------------
+    // ***************** Assuming the tube is whole.*********************
 
-    // 1. CNT on graphene
-    int j = 0;
-    double effectiveNum = 0;
+	// ----Placing the tube for easy reach of desired configurations -----
 
     Rotate(tube, tubeN, 3, (M_PI/6) - teta); // rotation around the z axis (spinning)
 	//RotateShift(tube, tubeN, rotateAngle, shiftAngle, ILD + radius); // rotation around the 
 	 																 // tube's axis.
 
-	// Calculating the RI max															
+    // Setting the correct x and yshifts of desired configurations:
+    // 1. CNT on graphene: maximal at AA, minimal at AB.
+    // 2. BNNT on h-BN	 : maximal at AA. minimal at AA' (All N on B)
+    // 3. Heterojunctions: maximal at AA (all eclipsed), minimal when half
+    // 	  of the C atoms are atop B atoms and the rest are above hexagon centers
+    //	  of the h-BN lattice.
+
+    double xShiftRIMax = 0.0;
+    double xShiftRIMin = 0.0;
+    double yShiftRIMax = 0.0;
+    double yShiftRIMin = 0.0;
+    double rotationAngleRIMin = 0.0;
+
+    if (tubeType == 0 && latticeType == 0) {      // 1. CNT on graphene
+		//RIMax
+		xShiftRIMax = 0.0;
+		yShiftRIMax = 0.0;
+
+		//RIMin
+    	xShiftRIMin = CNT_BL_HOMO;
+    	yShiftRIMin = 0.0;
+	}
+	else if (tubeType == 1 && latticeType == 1) { // 2. BN tube on BN lattice
+		//RIMax
+		xShiftRIMax = 0.0;
+		yShiftRIMax = 0.0;
+
+		//RIMin
+    	rotationAngleRIMin = M_PI/3;
+    	xShiftRIMin = -BN_LATTICE_BL_HOMO;
+    	yShiftRIMin = 0.0;
+	}
+	else if (tubeType == 0 && latticeType ==1) {  // 3. Heterojunctions: CNT on BN lattice
+		//RIMax
+		xShiftRIMax = 0.0;
+		yShiftRIMax = 0.0;
+
+		//RIMin
+    	xShiftRIMin = BN_LATTICE_BL_HETERO;
+    	yShiftRIMin = 0.0;
+	}
+	else {  									  // 3. Heterojunctions: BNNT on graphene
+		xShiftRIMax = 0.0;
+		yShiftRIMax = 0.0;
+
+    	xShiftRIMin = -BN_LATTICE_BL_HETERO;
+    	yShiftRIMin = 0.0;
+	}									  
+	
+
+    // ----------------- Calculating RIMax and RIMin ----------------------
+    int j = 0;
+    double effectiveNum = 0;
+
+	// Calculating the RI max
 	for (j = 0; j < tubeN; j++)
 	{
 		effectiveNum = exp( EXPNORM * (ILD - tube[j].z) / (RND - ILD) ); // find weight of atom
 		if (tube[j].z < MAX_HEIGHT)
 		{
-			RIMax = RIMax + effectiveNum * FindInteracting(tube[j], 0.0, 0.0, latticeType);
+			RIMax = RIMax + effectiveNum * FindInteracting(tube[j], xShiftRIMax, yShiftRIMax,
+														   latticeType);
 		}
 	}
 
-	// Calculating the RI min															
+	// Calculating the RI min
+	Rotate(tube, tubeN, 3, rotationAngleRIMin);
 	for (j = 0; j < tubeN; j++)
 	{
 		effectiveNum = exp( EXPNORM * (ILD - tube[j].z) / (RND - ILD) ); // find weight of atom
 		if (tube[j].z < MAX_HEIGHT)
 		{
-			RIMin = RIMin + effectiveNum * FindInteracting(tube[j], CNT_BL_HOMO, 0.0, latticeType);
+			RIMin = RIMin + effectiveNum * FindInteracting(tube[j], xShiftRIMin, yShiftRIMin,
+														   latticeType);
 		}
 	}
 
 	// Returning the tube to the original location
-	Rotate(tube, tubeN, 3, -((M_PI/6) - teta));
-
-	// if (tubeType == 0 && latticeType == 0) // CNT on graphene
-	// {
-	// 	RIMax = effTubeNMax * WeightIntersection(MIN(M_PI * pow(RCGRAPHENE,2), M_PI * pow(RCCNT,2)));
-	// 	RIMin = effTubeNMin * WeightIntersection(MIN(M_PI * pow(RCGRAPHENE,2), M_PI * pow(RCCNT,2)) / 2);
-	// }
-	// else if (tubeType == 1 && latticeType ==1)
-	//  // BN tube on BN lattice
-	// {
-	// 	RIMax = effTubeNMax * (0.5 * MIN(M_PI * pow(RNLATTICE,2), M_PI * pow(RNTUBE,2)) +
-	// 						   // N lattice on N tube
-	// 						   0.5 * MIN(M_PI * pow(RBLATTICE,2), M_PI * pow(RBTUBE,2))); 
-	// 						   // B lattice on B tube
-	// 	// calculating the new effective number of atoms and the RIMin
-	// 	RIMin = effTubeNMin * (0.5 * MIN(M_PI * pow(RBLATTICE,2), M_PI * pow(RNTUBE,2)) +
-	// 						   // B lattice on N tube
-	// 					       0.5 * MIN(M_PI * pow(RNLATTICE,2), M_PI * pow(RBTUBE,2))); 
-	// 					       // N lattice on B tube
-	// }
-	// else if (tubeType == 1 && latticeType == 0) // BN tube on graphene
-	// {
-	// 	RIMax = effTubeNMax * (0.5 * MIN(M_PI * pow(RCGRAPHENE,2), M_PI * pow(RNTUBE,2)) +
-	// 						   // C graphene on N tube
-	// 						   0.5 * MIN(M_PI * pow(RCGRAPHENE,2), M_PI * pow(RBTUBE,2))); 
-	// 						   // C graphene on B tube
-
-	// 	RIMin = effTubeNMin * (0.5 * MIN(M_PI * pow(RCGRAPHENE,2), M_PI * pow(RBTUBE,2))); 
-	// 					       // B tube on C graphene
-	// }
-	// else // CNT on BN lattice
-	// {
-	// 	RIMax = effTubeNMax * (0.5 * MIN(M_PI * pow(RNLATTICE,2), M_PI * pow(RCCNT,2)) +
-	// 						   // N lattice on C tube
-	// 						   0.5 * MIN(M_PI * pow(RBLATTICE,2), M_PI * pow(RCCNT,2))); 
-
-
-	// 	RIMin = effTubeNMin * (0.5 * MIN(M_PI * pow(RCCNT,2), M_PI * pow(RBLATTICE,2)));
-	// 						   // B lattice on C tube
-	// }
+	Rotate(tube, tubeN, 3, -((M_PI/6) - teta) + rotationAngleRIMin);
 
 	
 //********************** Step 5 - Calculate RI *********************************
