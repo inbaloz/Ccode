e568047840438dcdcd11de177dfad9c7290928ca
inbaloz <inbaloz@mail.tau.ac.il>
2015-10-09 10:20:31 +0300

    handling other folders in collect spe

--- collect_spe.py
+++ collect_spe.py
@@ -1,9 +1,17 @@
 from glob import glob
 import os
+import sys
+
 import re
 
-path = r'/home/inbaloz/Projects/NT_on_surfaces/interlayer_potential_Itai/'
-g = glob(path+"SPE*dat")
+path = r'/home/inbaloz/Projects/NT_on_surfaces/interlayer_potential_Itai'
+
+if len(sys.argv) > 1:
+	path += sys.argv[1]
+
+
+
+g = glob(path+"/SPE*dat")
 g.sort(key=lambda x: int(x.split(".")[-2]))
 print g
 l = [open(i,'rb').read() for i in g]
